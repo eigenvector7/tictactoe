483498bd9557f1c8190cedf4ba640867d56e0305
nishLBC <nishchal.chaudhary@longbeach.gov>
2020-09-17 00:06:59 -0700

    Updates

---
+++ precache-manifest.cbe9e79cb5894c52550d209bf20032e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64d9e6f1eeb3927c9c75f075afc1eff",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "a74ce226b7316a2ed7c6",
+    "url": "/tictactoe/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "b8fa3070a585d312d432",
+    "url": "/tictactoe/static/js/2.12295e38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.12295e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74ce226b7316a2ed7c6",
+    "url": "/tictactoe/static/js/main.932e4b0e.chunk.js"
+  },
+  {
+    "revision": "2cc4b16d50bca9235cae",
+    "url": "/tictactoe/static/js/runtime-main.155fd5f7.js"
+  }
+]);
